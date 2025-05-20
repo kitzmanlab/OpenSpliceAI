e8d2e9c54d9f495828cc48d36e71d451763fc12c
kitzmanj <kitzmanj@hk-ap-ps1a.med.umich.edu>
2025-05-20 15:38:45 -0400

    fix version issue

--- openspliceai/variant/utils.py
+++ openspliceai/variant/utils.py
@@ -114,7 +114,7 @@ def load_pytorch_models(model_path, CL):
     for state_dict in models:
         try: 
             model, params = load_model(device, CL) # loads new SpliceAI model with correct hyperparams
-            model.load_state_dict(state_dict)      # loads state dict
+            model.load_state_dict(state_dict, strict=False)      # loads state dict
             model = model.to(device)               # puts model on device
             model.eval()                           # puts model in evaluation mode
             loaded_models.append(model)            # appends model to list of loaded models  
